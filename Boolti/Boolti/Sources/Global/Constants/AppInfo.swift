add929e4b8d380140ab8048d9fb0819b9677e78a
wngus4296 <wngus4296@gmail.com>
2024-01-23 11:57:02 +0900

    chore: AppInfo singleton으로 변경

--- Boolti/Boolti/Sources/Global/Constants/AppInfo.swift
+++ Boolti/Boolti/Sources/Global/Constants/AppInfo.swift
@@ -8,11 +8,18 @@
 import Foundation
 
 struct AppInfo {
-    static let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-    static let bundleIdentifier = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String
-    static let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+    static let shared = AppInfo()
+    
+    private init() { }
+    
+    var appVersion: String {
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0.0"
+    }
+    
+    let bundleIdentifier = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String
+    let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
     
     // TODO: - 현재는 카카오톡 id, 애플에서 앱 아이디 발급 받고 수정 필요
     private static let appId = "362057947"
-    static let booltiAppStoreLink =  "itms-apps://itunes.apple.com/app/app-store/\(appId)"
+    let booltiAppStoreLink =  "itms-apps://itunes.apple.com/app/app-store/\(appId)"
 }
