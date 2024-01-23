81bb3481b1c42adbad39d81ae579f6b0b354eda8
longlivedrgn <kimye9808@naver.com>
2024-01-23 21:40:40 +0900

    feat: LoginViewModel 구현
    - Kakao Login API 호출

--- Boolti/Boolti/Sources/UILayer/Login/LoginViewController.swift
+++ Boolti/Boolti/Sources/UILayer/Login/LoginViewController.swift
@@ -6,15 +6,25 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class LoginViewController: UIViewController {
 
     private let viewModel: LoginViewModel
+    private let disposeBag = DisposeBag()
+
+    private let kakaoLoginButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "kakao_login_medium_wide"), for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .darkGray
-        print("View")
+        self.configureUI()
+        self.bind()
     }
 
     init(viewModel: LoginViewModel) {
@@ -25,5 +35,20 @@ class LoginViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    private func configureUI() {
+        self.view.addSubview(self.kakaoLoginButton)
+        self.kakaoLoginButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+
+    private func bind() {
+        self.kakaoLoginButton.rx.tap
+            .asDriver()
+            .drive(onNext: { _ in
+                self.viewModel.loginKakao()
+            })
+            .disposed(by: self.disposeBag)
+    }
 }
--- Boolti/Boolti/Sources/UILayer/Login/LoginViewModel.swift
+++ Boolti/Boolti/Sources/UILayer/Login/LoginViewModel.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import RxSwift
+import KakaoSDKUser
+import KakaoSDKAuth
 
 final class LoginViewModel {
 
@@ -16,5 +19,33 @@ final class LoginViewModel {
         self.authAPIService = authAPIService
         self.socialLoginAPIService = socialLoginAPIService
     }
+    // TODO: Input Ouput 모델로 변경할 예정!..
+    func loginKakao() {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            self.loginWithKakaoApp()
+        } else {
+            self.loginWithKakaoAccount()
+        }
+    }
+
+    func loginWithKakaoApp() {
+        UserApi.shared.loginWithKakaoTalk { oauthToken, error in
+            guard error != nil else { return }
+
+            let accessToken = oauthToken?.accessToken
+
+            print("loginWithKakaoApp 성공!..")
+        }
+    }
+
+    func loginWithKakaoAccount() {
+        UserApi.shared.loginWithKakaoAccount { oauthToken, error in
+            guard error != nil else { return }
+
+            print("loginWithKakaoApp 성공!..")
+        }
+    }
+
+
 
 }
