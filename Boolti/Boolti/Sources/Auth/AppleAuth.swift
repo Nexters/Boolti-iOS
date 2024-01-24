ab5bb5bafd9152948de40b02905e9bfa06ef223e
longlivedrgn <kimye9808@naver.com>
2024-01-24 21:53:40 +0900

    feat: Apple Login 구현
    - ASAuthorizationController Delegate Proxy 구현

--- Boolti/Boolti/Sources/Auth/AppleAuth.swift
+++ Boolti/Boolti/Sources/Auth/AppleAuth.swift
@@ -6,11 +6,28 @@
 //
 
 import RxSwift
+import AuthenticationServices
 
-struct AppleAuth: OAuth {
+class AppleAuth: NSObject, OAuth {
+
     func authorize() -> Observable<OAuthResponse> {
-        return Observable<OAuthResponse>.create { observer in
-            return Disposables.create()
-        }    }
+        let provider = ASAuthorizationAppleIDProvider()
+        let request = provider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.performRequests()
+        return authorizationController.rx.identityTokenDelegate
+          .map { identityToken in
+            guard let identityToken = identityToken else { throw Error.tokenDidNotFetched }
+              return OAuthResponse(accessToken: identityToken, provider: .apple)
+          }
+          .asObservable()
+          .compactMap { $0 }
+    }
 
+    enum Error: LocalizedError {
+      case tokenDidNotFetched
+
+      var errorDescription: String? { "애플과 연결을 실패했습니다." }
+    }
 }
