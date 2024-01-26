debe0a7c0d55a51c596f81c0bf7348825ad82fb5
JuHyeon Byun <58043306+wngus4296@users.noreply.github.com>
2024-01-26 11:30:47 +0900

    Feature/#20 navigation view 구현 (#24)
    * feat: 공통으로 쓰이는 네비게이션 바 구현
    
    * style: 네이밍 변경
    
    * style: button tap func 네이밍 변경
    
    Co-authored-by: Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>

---
+++ Boolti/Boolti/Sources/Global/UIElements/BooltiNavigationView.swift
@@ -0,0 +1,109 @@
+//
+//  BooltiNavigationView.swift
+//  Boolti
+//
+//  Created by Juhyeon Byun on 1/26/24.
+//
+
+import UIKit
+import RxCocoa
+import SnapKit
+
+final class BooltiNavigationView: UIView {
+    
+    enum NavigationType {
+        case payment
+        case concertDetail
+    }
+    
+    // MARK: UI Component
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .subhead2
+        label.textColor = .grey10
+        return label
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(.back, for: .normal)
+        return button
+    }()
+    
+    // MARK: Init
+    
+    init(type: NavigationType) {
+        super.init(frame: .zero)
+        
+        self.configureDefaultUI()
+        
+        switch type {
+        case .payment: self.configurePaymentUI()
+        case .concertDetail: self.configureConcertDetailUI()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+// MARK: - UI
+
+extension BooltiNavigationView {
+    
+    private func configureDefaultUI() {
+        self.backgroundColor = .grey95
+        
+        self.addSubview(backButton)
+        
+        self.configureDefaultConstraints()
+    }
+    
+    private func configureDefaultConstraints() {
+        self.snp.makeConstraints { make in
+            make.height.equalTo(88)
+        }
+    }
+    
+    private func configurePaymentUI() {
+        self.addSubview(backButton)
+        self.addSubview(titleLabel)
+        
+        self.configureBackButtonConstraints()
+        self.configureTitleConstraints()
+        
+        self.titleLabel.text = "결제하기"
+    }
+    
+    private func configureConcertDetailUI() {
+        self.addSubview(backButton)
+        
+        self.configureBackButtonConstraints()
+    }
+    
+    private func configureBackButtonConstraints() {
+        self.backButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(10)
+            make.left.equalToSuperview().inset(20)
+            make.height.width.equalTo(24)
+        }
+    }
+    
+    private func configureTitleConstraints() {
+        self.titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(self.backButton.snp.right).offset(4)
+            make.centerY.equalTo(self.backButton.snp.centerY)
+        }
+    }
+}
+
+// MARK: - Methods
+
+extension BooltiNavigationView {
+    
+    func backButtonDidTapped() -> Signal<Void> {
+        return backButton.rx.tap.asSignal()
+    }
+}
