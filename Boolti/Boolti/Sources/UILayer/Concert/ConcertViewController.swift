2664e996abfb05b41f820c7c7da9f0788db7ac4e
wngus4296 <wngus4296@gmail.com>
2024-01-26 14:59:35 +0900

    chore: 필요없는 코드 삭제

--- Boolti/Boolti/Sources/UILayer/Concert/ConcertViewController.swift
+++ Boolti/Boolti/Sources/UILayer/Concert/ConcertViewController.swift
@@ -9,21 +9,11 @@ import UIKit
 import SnapKit
 
 final class ConcertViewController: UIViewController {
-
-    private let navigationView: BooltiNavigationView = {
-        let view = BooltiNavigationView(type: .payment)
-        return view
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // TODO: 화면 넘어가는 거 확인용. 나중에 지워야함!
         self.view.backgroundColor = .yellow
-        
-        self.view.addSubviews([navigationView])
-        self.navigationView.snp.makeConstraints { make in
-            make.top.left.right.equalToSuperview()
-        }
     }
 }
