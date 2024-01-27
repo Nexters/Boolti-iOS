db58fe20b630eb001709cc3210498af7bee778f3
wngus4296 <wngus4296@gmail.com>
2024-01-27 19:12:44 +0900

    feat: 선택한 티켓 데이터 연결

--- Boolti/Boolti/Sources/UILayer/Ticket/TicketSelection/TicketSelectionViewModel.swift
+++ Boolti/Boolti/Sources/UILayer/Ticket/TicketSelection/TicketSelectionViewModel.swift
@@ -16,15 +16,16 @@ final class TicketSelectionViewModel {
     private let disposeBag = DisposeBag()
     
     struct Input {
+        let selectedTickets = BehaviorRelay<[TicketEntity]>(value: [])
     }
 
     struct Output {
         // TODO: 데이터 체크용. api 연결후 []로 초기화 예정
         let tickets = BehaviorRelay<[TicketEntity]>(value: [.init(id: 0, name: "초청 티켓", price: 0, inventory: 100),
-//                                                            .init(id: 1, name: "일반 티켓 A", price: 3000, inventory: 0),
-//                                                            .init(id: 2, name: "일반 티켓 B", price: 5000, inventory: 300),
-//                                                            .init(id: 2, name: "일반 티켓 C", price: 15000, inventory: 10)])
-                                                            ])
+                                                            .init(id: 1, name: "일반 티켓 A", price: 3000, inventory: 0),
+                                                            .init(id: 2, name: "일반 티켓 B", price: 5000, inventory: 300),
+                                                            .init(id: 2, name: "일반 티켓 C", price: 15000, inventory: 10)])
+        let totalPrice = BehaviorSubject<Int>(value: 0)
     }
 
     let input: Input
@@ -35,5 +36,21 @@ final class TicketSelectionViewModel {
     init() {
         self.input = Input()
         self.output = Output()
+        
+        self.bindInputs()
+    }
+}
+
+// MARK: - Methods
+
+extension TicketSelectionViewModel {
+    
+    private func bindInputs() {
+        self.input.selectedTickets
+            .map { ticket in
+                return ticket.reduce(0) { $0 + $1.price }
+            }
+            .bind(to: self.output.totalPrice)
+            .disposed(by: self.disposeBag)
     }
 }
