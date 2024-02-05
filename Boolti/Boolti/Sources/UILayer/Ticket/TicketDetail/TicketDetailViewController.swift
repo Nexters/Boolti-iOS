57e9b9baa47d2566012f3d65310eb75f5eb0a40e
longlivedrgn <kimye9808@naver.com>
2024-02-05 18:25:02 +0900

    feat: 입장 코드 버튼 구현

--- Boolti/Boolti/Sources/UILayer/Ticket/TicketDetail/TicketDetailViewController.swift
+++ Boolti/Boolti/Sources/UILayer/Ticket/TicketDetail/TicketDetailViewController.swift
@@ -33,6 +33,19 @@ class TicketDetailViewController: UIViewController {
         return stackView
     }()
 
+    private let entryCodeView: UIView = {
+        let view = UIView()
+        return view
+    }()
+
+    private let entryCodeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("입장 코드 입력하기", for: .normal)
+        button.setUnderline(font: .pretendardR(14), textColor: .grey50)
+
+        return button
+    }()
+
     private lazy var ticketDetailView = TicketDetailView(item: self.ticketItem)
     private let seperatingBlankView = UIView()
     private let reversalPolicyView = ReversalPolicyView()
@@ -58,8 +71,10 @@ class TicketDetailViewController: UIViewController {
 
     private func configureUI() {
         self.view.backgroundColor = .black
+
         self.view.addSubviews([self.navigationBar, self.scrollView])
         self.scrollView.addSubview(self.contentStackView)
+        self.entryCodeView.addSubview(self.entryCodeButton)
 
         self.navigationBar.snp.makeConstraints { make in
             make.top.horizontalEdges.equalToSuperview()
@@ -75,6 +90,18 @@ class TicketDetailViewController: UIViewController {
             make.height.equalTo(20)
         }
 
+        self.entryCodeView.snp.makeConstraints { make in
+            make.height.equalTo(70)
+        }
+
+        self.entryCodeButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+
+        self.entryCodeButton.snp.makeConstraints { make in
+            make.height.equalTo(60)
+        }
+
         self.contentStackView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
             make.width.equalToSuperview()
@@ -83,7 +110,8 @@ class TicketDetailViewController: UIViewController {
         self.contentStackView.addArrangedSubviews([
             self.ticketDetailView,
             self.seperatingBlankView,
-            self.reversalPolicyView
+            self.reversalPolicyView,
+            self.entryCodeView
         ])
     }
 
