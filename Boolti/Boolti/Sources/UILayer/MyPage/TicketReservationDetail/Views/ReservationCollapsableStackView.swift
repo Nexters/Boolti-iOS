2a20844d64499f1e88ae30d8cb431e3d1479db84
longlivedrgn <kimye9808@naver.com>
2024-02-12 02:34:24 +0900

    feat: ReservationCollapsableStackView 및 ReservationHorizontalStackView 구현

---
+++ Boolti/Boolti/Sources/UILayer/MyPage/TicketReservationDetail/Views/ReservationCollapsableStackView.swift
@@ -0,0 +1,83 @@
+//
+//  ReservationCollapsableStackView.swift
+//  Boolti
+//
+//  Created by Miro on 2/12/24.
+//
+
+import UIKit
+
+final class ReservationCollapsableStackView: UIStackView {
+
+    private let titleView: UIView = {
+        let view = UIView()
+        return view
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .subhead2
+        label.textColor = .grey10
+        return label
+    }()
+
+    private let viewCollapseButton: UIButton = {
+        let button = UIButton()
+        button.setImage(.chevronDown, for: .normal)
+        button.setImage(.chevronUp, for: .selected)
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    init(title: String, contentViews: [ReservationHorizontalStackView]) {
+
+        super.init(frame: .zero)
+        self.configreUI(title: title, contentViews: contentViews)
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configreUI(title: String, contentViews: [ReservationHorizontalStackView]) {
+
+        self.axis = .vertical
+        self.alignment = .center
+        self.backgroundColor = .grey90
+        self.titleLabel.text = title
+        
+        let subviews = [self.titleView] + contentViews
+        self.addArrangedSubviews(subviews)
+
+        self.titleView.addSubviews([
+            self.titleLabel, self.viewCollapseButton
+        ])
+
+        self.configureConstraints()
+    }
+
+    private func configureConstraints() {
+
+        guard let window = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return
+        }
+
+        self.titleView.snp.makeConstraints { make in
+            make.width.equalTo(window.screen.bounds.width)
+            make.height.equalTo(66)
+        }
+
+        self.titleLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().inset(20)
+        }
+
+        self.viewCollapseButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview().inset(20)
+        }
+    }
+}
---
+++ Boolti/Boolti/Sources/UILayer/MyPage/TicketReservationDetail/Views/ReservationHorizontalStackView.swift
@@ -0,0 +1,94 @@
+//
+//  ReservationHorizontalStackView.swift
+//  Boolti
+//
+//  Created by Miro on 2/12/24.
+//
+
+import UIKit
+
+enum ReservationContentAlignment {
+    case left
+    case right
+}
+
+final class ReservationHorizontalStackView: UIStackView {
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendardR(16)
+        label.textColor = .grey30
+        label.textAlignment = .left
+
+        return label
+    }()
+
+    private let contentLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendardR(16)
+        label.textColor = .grey15
+
+        return label
+    }()
+
+    init(title: String, alignment: ReservationContentAlignment) {
+        super.init(frame: .zero)
+
+        self.configureUI(title: title, alignment: alignment)
+        self.configureConstraints()
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureUI(title: String, alignment: ReservationContentAlignment) {
+
+        self.titleLabel.text = title
+
+        self.axis = .horizontal
+        self.alignment = .fill
+        self.spacing = 20
+
+        self.addArrangedSubviews([
+            self.titleLabel,
+            self.contentLabel
+        ])
+    }
+
+    func setData(_ content: String) {
+        self.contentLabel.text = content
+    }
+
+    private func configureAlignment(_ alignment: ReservationContentAlignment) {
+
+        switch alignment {
+        case .left:
+            self.contentLabel.textAlignment = .left
+        case .right:
+            self.contentLabel.textAlignment = .right
+        }
+    }
+
+    private func configureConstraints() {
+
+        guard let window = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return
+        }
+
+        self.snp.makeConstraints { make in
+            make.width.equalTo(window.screen.bounds.width - 40)
+        }
+
+        self.titleLabel.snp.makeConstraints { make in
+            make.width.equalTo(80)
+            make.height.equalTo(32)
+        }
+
+        self.contentLabel.snp.makeConstraints { make in
+            make.width.equalTo(235)
+            make.height.equalTo(32)
+        }
+    }
+
+}
