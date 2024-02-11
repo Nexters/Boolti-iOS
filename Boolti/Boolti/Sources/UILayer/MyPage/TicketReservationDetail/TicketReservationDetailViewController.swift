28a46e55629f1d9970c09e61b3eec79462433762
longlivedrgn <kimye9808@naver.com>
2024-02-12 02:23:18 +0900

    feat: ViewController scollview와 navigationbar 구현

--- Boolti/Boolti/Sources/UILayer/MyPage/TicketReservationDetail/TicketReservationDetailViewController.swift
+++ Boolti/Boolti/Sources/UILayer/MyPage/TicketReservationDetail/TicketReservationDetailViewController.swift
@@ -7,14 +7,46 @@
 
 import UIKit
 
+import RxSwift
+import RxAppState
+import RxCocoa
+
 final class TicketReservationDetailViewController: BooltiViewController {
 
     private let viewModel: TicketReservationDetailViewModel
+    private let disposeBag = DisposeBag()
+
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .black
+        scrollView.showsVerticalScrollIndicator = false
+
+        return scrollView
+    }()
+
+    private let contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .leading
+
+        return stackView
+    }()
+
+    private let reservationIDLabel: BooltiPaddingLabel = {
+        let label = BooltiPaddingLabel(padding: UIEdgeInsets(top: 12, left: 20, bottom: 0, right: 0))
+        label.textColor = .grey50
+        label.font = .pretendardR(14)
+
+        return label
+    }()
 
     init(viewModel: TicketReservationDetailViewModel) {
         self.viewModel = viewModel
         super.init()
     }
+
+    private let navigationBar = BooltiNavigationBar(type: .ticketReservationDetail)
 
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = true
@@ -27,6 +59,7 @@ final class TicketReservationDetailViewController: BooltiViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureUI()
+        self.configureConstraints()
     }
 
     required init?(coder: NSCoder) {
@@ -36,5 +69,27 @@ final class TicketReservationDetailViewController: BooltiViewController {
     private func configureUI() {
         self.view.backgroundColor = .red
         self.navigationController?.navigationBar.isHidden = true
+
+        self.view.addSubviews([
+            self.navigationBar,
+            self.scrollView
+        ])
+
+        self.scrollView.addSubview(self.contentStackView)
+    }
+
+    private func configureConstraints() {
+        self.navigationBar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview()
+        }
+
+        self.scrollView.snp.makeConstraints { make in
+            make.top.equalTo(self.navigationBar.snp.bottom).offset(50)
+            make.bottom.horizontalEdges.equalToSuperview()
+        }
+
+        self.contentStackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
