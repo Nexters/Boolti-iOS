34dd427e93ed0880856c0a50fadb038aa4a59633
juhyeon <wngus4296@swu.ac.kr>
2024-02-13 01:16:20 +0900

    fix: 포스터 radius안잘리는 오류 수정

--- Boolti/Boolti/Sources/UILayer/Concert/TicketingCompletion/Components/Views/ReservedTicketView.swift
+++ Boolti/Boolti/Sources/UILayer/Concert/TicketingCompletion/Components/Views/ReservedTicketView.swift
@@ -15,6 +15,7 @@ final class ReservedTicketView: UIView {
         let view = UIImageView()
         view.backgroundColor = .grey30
         view.layer.cornerRadius = 4
+        view.clipsToBounds = true
         view.layer.borderWidth = 1
         view.layer.borderColor = UIColor.grey80.cgColor
         return view
