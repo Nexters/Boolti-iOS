7e443fddf3b32c3b99a6589fd44a5048c68e37f7
longlivedrgn <kimye9808@naver.com>
2024-02-13 12:20:55 +0900

    feat: NavigationBar 구현

--- Boolti/Boolti/Sources/UILayer/MyPage/TicketRefund/TicketRefundReason/TicketRefundReasonViewController.swift
+++ Boolti/Boolti/Sources/UILayer/MyPage/TicketRefund/TicketRefundReason/TicketRefundReasonViewController.swift
@@ -11,6 +11,8 @@ class TicketRefundReasonViewController: BooltiViewController {
 
     private let viewModel: TicketRefundReasonViewModel
 
+    private let navigationBar = BooltiNavigationBar(type: .defaultUI(backButtonTitle: "환불 요청하기"))
+
     init(viewModel: TicketRefundReasonViewModel) {
         self.viewModel = viewModel
         super.init()
@@ -18,5 +20,30 @@ class TicketRefundReasonViewController: BooltiViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationController?.navigationBar.isHidden = true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureUI()
+        self.configureConstraints()
+    }
+
+    private func configureUI() {
+        self.view.addSubviews([
+            self.navigationBar
+        ])
+        self.view.backgroundColor = .grey95
+    }
+
+    private func configureConstraints() {
+        self.navigationBar.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+        }
     }
 }
