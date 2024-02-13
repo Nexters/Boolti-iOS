997e108f1c95023166aa1018b9db8877d5ab82de
wngus4296 <wngus4296@gmail.com>
2024-02-13 14:20:13 +0900

    fix: dto 디코딩 오류 수정

--- Boolti/Boolti/Sources/UILayer/MyPage/TicketReservations/TicketReservationsViewModel.swift
+++ Boolti/Boolti/Sources/UILayer/MyPage/TicketReservations/TicketReservationsViewModel.swift
@@ -39,7 +39,6 @@ final class TicketReservationsViewModel {
         self.input.viewWillAppearEvent
             .flatMap { self.fetchTicketReservations() }
             .subscribe(with: self) { owner, ticketReservations in
-                // 카카오 로그인일 때만 여기 안들어옴!
                 owner.output.tickerReservations.accept(ticketReservations)
             }
             .disposed(by: self.disposeBag)
