95fe84decdae805c1e516719ab0a29e99f79c5c1
JuHyeon Byun <58043306+wngus4296@users.noreply.github.com>
2024-02-15 23:06:20 +0900

    Fix/#104 공연 내용 전체보기 textView로 변경 (스크롤 가능) (#109)

---
+++ Boolti/Boolti/Sources/Global/Extensions/UITextView+.swift
@@ -0,0 +1,30 @@
+//
+//  UITextView+.swift
+//  Boolti
+//
+//  Created by Juhyeon Byun on 2/15/24.
+//
+
+import UIKit
+
+extension UITextView {
+
+    /// 행간 조정 메서드
+    func setLineSpacing(lineSpacing: CGFloat) {
+        if let attributedText = self.attributedText, let font = self.font, let textColor = self.textColor {
+            let mutableAttributedText = NSMutableAttributedString(attributedString: attributedText)
+
+            let style = NSMutableParagraphStyle()
+            style.lineSpacing = lineSpacing
+            style.lineBreakStrategy = .hangulWordPriority
+
+            mutableAttributedText
+                .addAttributes([.paragraphStyle: style,
+                                                 .font: font,
+                                                 .foregroundColor: textColor],
+                               range: NSMakeRange(0, mutableAttributedText.length))
+
+            self.attributedText = mutableAttributedText
+        }
+    }
+}
