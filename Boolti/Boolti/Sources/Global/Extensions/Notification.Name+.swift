c0df0d89f201317673dcaa972a10465d3551d06a
longlivedrgn <kimye9808@naver.com>
2024-02-18 21:52:11 +0900

    feat: Refresh 토큰 만료 시 앱 재시작 로직 구현

---
+++ Boolti/Boolti/Sources/Global/Extensions/Notification.Name+.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.Name+.swift
+//  Boolti
+//
+//  Created by Miro on 2/18/24.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let refreshTokenHasExpired = Notification.Name("refreshTokenHasExpired")
+}
