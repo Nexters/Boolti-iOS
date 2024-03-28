7f63cae92e1fbb6bfae00d640ff19564c1fa02b2
longlivedrgn <kimye9808@naver.com>
2024-03-29 01:10:59 +0900

    feat: NotificationMessageTitle 수정

--- Boolti/Boolti/Sources/Global/Enums/NotificationMessageTitle.swift
+++ Boolti/Boolti/Sources/Global/Enums/NotificationMessageTitle.swift
@@ -11,13 +11,19 @@ enum NotificationMessageTitle {
 
     case didTicketIssued
     case concertWillStart
+    case didrefundCompleted
+    case despositRequest
 
     init?(_ rawValue: String){
         switch rawValue {
-        case "발권 완료": // 발권 완료
+        case "RESERVATION_COMPLETED": // 발권 완료
             self  = .didTicketIssued
-        case "입장 대기": // 입장 대기
+        case "ENTER_NOTIFICATION": // 입장 대기
             self = .concertWillStart
+        case "DEPOSIT_REQUEST": // 입금 요청
+            self = .despositRequest
+        case "REFUND_COMPLETED": // 환불 완료
+            self = .didrefundCompleted
         default:
             return nil
         }
@@ -29,6 +35,10 @@ enum NotificationMessageTitle {
             return 1
         case .concertWillStart:
             return 1
+        case .didrefundCompleted:
+            return 2
+        case .despositRequest:
+            return 0
         }
     }
 }
