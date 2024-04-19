181e68ffc8a20ea8076a6428a6f1359619daad76
longlivedrgn <kimye9808@naver.com>
2024-04-19 16:40:19 +0900

    feat: RemoteConfig 환불 정책 추가 및 AppInfo에 추가
    - ReversalPolicyView에서 AppInfo 값 활용하게 변경

--- Boolti/Boolti/Sources/UILayer/Splash/SplashViewModel.swift
+++ Boolti/Boolti/Sources/UILayer/Splash/SplashViewModel.swift
@@ -42,15 +42,24 @@ final class SplashViewModel {
         navigationDelegate.splashViewModel(self)
     }
     
-    func checkUpdateRequired() {
+    func checkRemoteConfig() {
         self.remoteConfig.fetchAndActivate(completionHandler: { status, error in
             if status == .error { return }
             
             guard let minVersion = self.remoteConfig.configValue(forKey: "MinVersion").stringValue else { return }
             let updateRequired = self.remoteConfig.configValue(forKey: "UpdateRequired").boolValue
-            
+            let jsonReversalPolicy = self.remoteConfig.configValue(forKey: "RefundPolicy").jsonValue
+            self.configureReversalPolicy(jsonStatements: jsonReversalPolicy)
+
             self.output.updateRequired.accept(updateRequired &&
                                        AppInfo.appVersion?.compare(minVersion, options: .numeric) == .orderedAscending)
         })
+    }
+
+    private func configureReversalPolicy(jsonStatements: Any?) {
+        guard let revesalPolicyStatements = jsonStatements as? [String] else { return }
+        let bulletAddedStatements = revesalPolicyStatements.map { "• \($0)" }
+        let reversalPolicy = bulletAddedStatements.joined(separator: "\n")
+        AppInfo.reversalPolicy = reversalPolicy
     }
 }
