a51682e0586da7d3edfd9636219361cafc4426cf
longlivedrgn <kimye9808@naver.com>
2024-05-09 00:31:01 +0900

    chore: 리모트 컨피그에 추가

--- Boolti/Boolti/Sources/UILayer/MyPage/TicketRefund/TicketRefundRequest/TicketRefundRequestViewController.swift
+++ Boolti/Boolti/Sources/UILayer/MyPage/TicketRefund/TicketRefundRequest/TicketRefundRequestViewController.swift
@@ -15,6 +15,10 @@ import RxKeyboard
 
 final class TicketRefundRequestViewController: BooltiViewController {
 
+    private enum Constants {
+        static let booltiDomain = "@스튜디오불티"
+    }
+
     typealias ReservationID = String
     typealias ReasonText = String
 
@@ -58,18 +62,12 @@ final class TicketRefundRequestViewController: BooltiViewController {
 
     // 취소/환불 규정
     private lazy var reversalPolicyTitlelabel = self.makeTitleLabel(title: "취소/환불 규정")
-    // Remote Config로 넘어갈 예정
     private let reversalPolicyLabel: BooltiPaddingLabel = {
         let label = BooltiPaddingLabel(padding: UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0))
-        label.text = """
-        • 티켓 판매 기간 내 발권 취소 및 환불은 서비스 내 처리가 가능하며, 판매 기간 이후에는 주최자에게 직접 연락 바랍니다.
-        • 티켓 판매 기간 내 환불 신청은 발권 후 마이 > 예매 내역 > 예매 상세에서 가능합니다.
-        • 계좌 이체를 통한 환불은 환불 계좌 정보가 필요하며 영업일 기준 약 1~2일이 소요됩니다.
-        • 환불 수수료는 부과되지 않습니다.
-        • 기타 사항은 카카오톡 채널 @스튜디오불티로 문의 부탁드립니다.
-        """
+        label.text = AppInfo.reversalPolicy
         label.numberOfLines = 0
         label.setHeadIndent()
+        label.setUnderLine(to: Constants.booltiDomain)
         label.font = .body1
         label.textColor = .grey50
         return label
