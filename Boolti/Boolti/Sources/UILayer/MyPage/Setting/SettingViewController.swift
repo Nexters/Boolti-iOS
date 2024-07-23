e4c5634fa524913189dfe97e1b4c8ac3730c66c9
JuHyeon Byun <wngus4296@swu.ac.kr>
2024-07-23 15:48:01 +0900

    feat: 마이페이지 화면에 계정 설정 탭 추가

---
+++ Boolti/Boolti/Sources/UILayer/MyPage/Setting/SettingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingViewController.swift
+//  Boolti
+//
+//  Created by Juhyeon Byun on 7/23/24.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+final class SettingViewController: BooltiViewController {
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+    
+}
