17cfcac4c40549cc5d059b63fd1d318c1116b1de
JuHyeon Byun <wngus4296@swu.ac.kr>
2024-08-24 16:33:07 +0900

    feat: 마이페이지 프로필 상단 메인 프로필뷰 구현

--- Boolti/Boolti/Sources/UILayer/MyPage/Profile/ProfileViewController.swift
+++ Boolti/Boolti/Sources/UILayer/MyPage/Profile/ProfileViewController.swift
@@ -17,6 +17,29 @@ final class ProfileViewController: BooltiViewController {
     private let disposeBag = DisposeBag()
     private let viewModel: ProfileViewModel
     
+    // MARK: UI Components
+    
+    private let navigationBar = BooltiNavigationBar(type: .backButtonWithTitle(title: "프로필"))
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.delegate = self
+        scrollView.addSubviews([self.stackView])
+        
+        return scrollView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.addArrangedSubviews([self.profileMainView])
+        
+        return stackView
+    }()
+    
+    private let profileMainView = ProfileMainView()
+
     // MARK: Initailizer
     
     init(viewModel: ProfileViewModel) {
@@ -39,8 +62,43 @@ final class ProfileViewController: BooltiViewController {
         super.viewDidLoad()
         
         self.configureUI()
+        self.bindUIComponents()
     }
     
+}
+
+// MARK: - Methods
+
+extension ProfileViewController {
+    
+    private func bindUIComponents() {
+        self.navigationBar.didBackButtonTap()
+            .emit(with: self) { owner, _ in
+                owner.navigationController?.popViewController(animated: true)
+            }
+            .disposed(by: self.disposeBag)
+        
+        self.profileMainView.didEditButtonTap()
+            .emit(with: self) { owner, _ in
+            }
+            .disposed(by: self.disposeBag)
+    }
+    
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension ProfileViewController: UIScrollViewDelegate {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        if offset <= self.profileMainView.frame.height {
+            self.navigationBar.setBackgroundColor(with: .grey90)
+        } else {
+            self.navigationBar.setBackgroundColor(with: .grey95)
+        }
+    }
+
 }
 
 // MARK: - UI
@@ -48,7 +106,26 @@ final class ProfileViewController: BooltiViewController {
 extension ProfileViewController {
     
     private func configureUI() {
-        self.view.backgroundColor = .red
+        self.view.addSubviews([self.navigationBar,
+                               self.scrollView])
+        self.navigationBar.setBackgroundColor(with: .grey90)
+        self.configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        self.navigationBar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview()
+        }
+        
+        self.scrollView.snp.makeConstraints { make in
+            make.top.equalTo(self.navigationBar.snp.bottom)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
+        
+        self.stackView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.edges.equalTo(self.scrollView)
+        }
     }
     
 }
---
+++ Boolti/Boolti/Sources/UILayer/MyPage/Profile/Views/ProfileMainView.swift
@@ -0,0 +1,128 @@
+//
+//  ProfileMainView.swift
+//  Boolti
+//
+//  Created by Juhyeon Byun on 8/24/24.
+//
+
+import UIKit
+
+import RxCocoa
+
+final class ProfileMainView: UIView {
+    
+    // MARK: UI Components
+
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .grey80
+        imageView.layer.cornerRadius = 35
+        imageView.clipsToBounds = true
+        imageView.layer.borderColor = UIColor.grey80.cgColor
+
+        return imageView
+    }()
+
+    private let nameLabel: BooltiUILabel = {
+        let label = BooltiUILabel()
+        label.font = .aggroM(24)
+        label.textColor = .grey10
+        label.text = "재미있는"
+
+        return label
+    }()
+    
+    private let descriptionLabel: BooltiUILabel = {
+        let label = BooltiUILabel()
+        label.font = .body3
+        label.textColor = .grey30
+        label.text = "재미있는 음악을 추구해요"
+
+        return label
+    }()
+    
+    private lazy var labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 2
+        stackView.addArrangedSubviews([self.nameLabel,
+                                       self.descriptionLabel])
+        
+        return stackView
+    }()
+    
+    private let editButton: UIButton = {
+        var config = UIButton.Configuration.plain()
+        config.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12)
+        config.title = "프로필 편집"
+        config.attributedTitle?.font = .pretendardR(12)
+        config.background.backgroundColor = .grey80
+        config.baseForegroundColor = .grey05
+        config.background.cornerRadius = 4
+        config.imagePadding = 6
+        
+        let button = UIButton(configuration: config)
+        button.setImage(.pencil, for: .normal)
+
+        return button
+    }()
+    
+    // MARK: Initailizer
+    
+    init() {
+        super.init(frame: .zero)
+        self.configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
+
+// MARK: - Methods
+
+extension ProfileMainView {
+    
+    func didEditButtonTap() -> Signal<Void> {
+        return self.editButton.rx.tap.asSignal()
+    }
+    
+}
+
+// MARK: - UI
+
+extension ProfileMainView {
+    
+    private func configureUI() {
+        self.backgroundColor = .grey90
+        self.layer.cornerRadius = 20
+        self.layer.maskedCorners = CACornerMask(
+            arrayLiteral: .layerMinXMaxYCorner, .layerMaxXMaxYCorner
+        )
+        self.addSubviews([self.profileImageView,
+                          self.labelStackView,
+                          self.editButton])
+        
+        self.configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        self.profileImageView.snp.makeConstraints { make in
+            make.size.equalTo(70)
+            make.top.equalToSuperview().inset(40)
+            make.leading.equalToSuperview().inset(20)
+        }
+
+        self.labelStackView.snp.makeConstraints { make in
+            make.top.equalTo(self.profileImageView.snp.bottom).offset(20)
+            make.horizontalEdges.equalToSuperview().inset(20)
+        }
+        
+        self.editButton.snp.makeConstraints { make in
+            make.top.equalTo(self.labelStackView.snp.bottom).offset(28)
+            make.leading.equalTo(self.labelStackView)
+            make.bottom.equalToSuperview().inset(32)
+        }
+    }
+}
