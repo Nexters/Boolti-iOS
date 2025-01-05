6bd3fd16368e5d63947fab7f8e794ef788ebbea0
JuHyeon Byun <wngus4296@swu.ac.kr>
2025-01-05 16:45:26 +0900

    chore: qr scanner text 변경

--- Boolti/Boolti/Sources/UILayer/MyPage/QRScanner/QRScannerList/QRScannerListViewController.swift
+++ Boolti/Boolti/Sources/UILayer/MyPage/QRScanner/QRScannerList/QRScannerListViewController.swift
@@ -20,7 +20,7 @@ final class QRScannerListViewController: BooltiViewController {
     
     // MARK: UI Component
 
-    private let navigationBar = BooltiNavigationBar(type: .backButtonWithTitle(title: "QR 스캔"))
+    private let navigationBar = BooltiNavigationBar(type: .backButtonWithTitle(title: "입장 확인"))
     
     private lazy var emtpyLabelStackView: UIStackView = {
         let stackView = UIStackView()
@@ -37,7 +37,7 @@ final class QRScannerListViewController: BooltiViewController {
         let label = BooltiUILabel()
         label.font = .headline1
         label.textColor = .grey05
-        label.text = "주최한 공연이 없어요"
+        label.text = "입장을 확인할 공연이 없어요"
         return label
     }()
     
@@ -46,7 +46,7 @@ final class QRScannerListViewController: BooltiViewController {
         label.numberOfLines = 0
         label.font = .body3
         label.textColor = .grey30
-        label.text = "공연을 주최하고 QR 스캐너로\n관객 입장을 관리해 보세요"
+        label.text = "공연을 주최하고 QR코드로\n관객 입장을 확인해 보세요 "
         label.setAlignment(.center)
         return label
     }()
